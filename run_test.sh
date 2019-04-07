c437d057b9fa68e8f21576408b8e99ce9cb7dcab
jing-q-xu <jing.q.xu@hotmail.com>
2019-04-07 21:08:01 +0800

    xml output

--- run_test.sh
+++ run_test.sh
@@ -7,7 +7,7 @@ for case_bin in `ls | grep "test_"`
 do
     echo ${case_bin}
     if [ -x ${case_bin} ];then
-        ./${case_bin} --gtest_output="xml:${case_bin}.xml";
+        ./${case_bin} --gtest_output="xml:../${case_bin}.xml";
         if [ $? -eq 1 ];then
             echo " ${case_bin} failed"
             result=1
